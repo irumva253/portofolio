2d71f14c999084939d445a21c08fbe9822c070b1
irumva253 <irumvapatrick253@gmail.com>
2023-01-28 17:58:56 +0200

    first commit

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php      
+    $host = "containers-us-west-167.railway.app";  
+    $user = "root";  
+    $password = 'FU67ldenYyvLkbMEHCPz';  
+    $db_name = "railway";  
+      
+    $con = mysqli_connect($host, $user, $password, $db_name);  
+    if(mysqli_connect_errno()) {  
+        die("Failed to connect with MySQL: ". mysqli_connect_error());  
+    }  
+?>  
